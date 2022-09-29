386b2c6f653e4faaf8f076ee7baa698e91c14d83
kad1kad <k.aliu@me.com>
2022-09-29 11:01:09 +0100

    add Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,21 @@
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
     <header className="">
-      <div className="flex items-center justify-end">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex items-center justify-end"
+      >
         <SocialIcon
-          className="cursor-pointer"
+          className="cursor-pointer flex"
           network="email"
           fgColor="black"
           bgColor="white"
@@ -15,7 +23,7 @@ function Header({}: Props) {
         <p className="hidden md:inline-flex text-sm text-black ">
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
