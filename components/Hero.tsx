65bc0879896fa2da353ee507071da9893bbaa5d4
kad1kad <k.aliu@me.com>
2022-10-13 21:04:02 +0100

    pull from sanity cms

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,38 @@
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { PageInfo } from "../typings";
 
-type Props = {};
+type Props = {
+  pageInfo: PageInfo;
+};
 
-function Hero({}: Props) {
+function Hero({ pageInfo }: Props) {
+  console.log(pageInfo);
   return (
     <motion.div
+      className="h-screen flex flex-col justify-center items-center"
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{
         duration: 1,
       }}
     >
-      <h1 className="text-4xl">Hi, I'm Kad.</h1>
-      <h2>Frontend Developer</h2>
+      <h1 className="text-6xl text-center font-extrabold md:text-8xl ">
+        Hi, I'm {pageInfo.name}.
+      </h1>
+      <h2 className="font-bold ">{pageInfo.role}</h2>
+
+      {/* <div className="pt-5 leading-7">
+        <Link href="#about">
+          <h4 className="heroLink">About</h4>
+        </Link>
+        <Link href="#skills">
+          <h4 className="heroLink">Skills</h4>
+        </Link>
+        <Link href="#projects">
+          <h4 className="heroLink">Projects</h4>
+        </Link>
+      </div> */}
     </motion.div>
   );
 }
