bf6761db3de5a06f0a4a16c56658d7bd2ba092c1
kad1kad <k.aliu@me.com>
2022-10-16 12:46:34 +0100

    add sanity schema

---
+++ sanity/schemas/pageInfo.js
@@ -0,0 +1,17 @@
+export default {
+  name: "pageInfo",
+  title: "PageInfo",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+  ],
+};
