7ca44484d8902cf94628cd9e2915956137d72df5
kad1kad <k.aliu@me.com>
2022-10-16 12:40:23 +0100

    extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,48 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        waves: "url('/img/waves.svg')",
+      },
+      animation: {
+        "gradient-x": "gradient-x 6s ease infinite",
+        "gradient-y": "gradient-y 6s ease infinite",
+        "gradient-xy": "gradient-xy 6s ease infinite",
+      },
+      keyframes: {
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center",
+          },
+        },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
