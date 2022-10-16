3521a537507279b5c6e4617c02cb7b9a119025ee
kad1kad <k.aliu@me.com>
2022-10-16 12:44:19 +0100

    add locomtive wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useRef } from "react";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const containerRef = useRef(null);
+  return (
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+        // ... all available Locomotive Scroll instance options
+      }}
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        <Component {...pageProps} />
+      </main>
+    </LocomotiveScrollProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
