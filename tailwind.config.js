5eafb4c3504b8ffc3895c341cf5bad4911a6da52
kad1kad <k.aliu@me.com>
2022-10-17 15:06:20 +0100

    slow down gradient animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "gradient-x": "gradient-x 6s ease infinite",
-        "gradient-y": "gradient-y 6s ease infinite",
-        "gradient-xy": "gradient-xy 6s ease infinite",
+        "gradient-x": "gradient-x 15s ease infinite",
+        "gradient-y": "gradient-y 15s ease infinite",
+        "gradient-xy": "gradient-xy 15s ease infinite",
       },
       keyframes: {
         "gradient-y": {
