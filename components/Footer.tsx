e3465f5414f43a9f1106de0c6216c8f06f1a5048
kad1kad <k.aliu@me.com>
2022-10-17 11:43:19 +0100

    add locomotive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,19 @@ type Props = {};
 
 function Footer({}: Props) {
   return (
-    <div className="py-10 flex flex-col items-center leading-10 h-44">
-      <p className="text-center font-light text-sm">
+    <div
+      className="py-10 flex flex-col items-center leading-10 h-44"
+      data-scroll-section
+    >
+      <p className="text-center font-light text-sm" data-scroll>
         Built with Next.js, TypeScript, TailwindCSS, Sanity CMS, Framer Motion.
       </p>
       <a href="mailto:k.aliu@me.com?body=Hi">
         <span className="">✉️</span>
-        <p className="hidden md:inline-flex ml-1 text-sm text-neutral-100 cursor-pointer ">
+        <p
+          className="hidden md:inline-flex ml-1 text-sm text-neutral-100 cursor-pointer"
+          data-scroll
+        >
           Get In Touch
         </p>
       </a>
