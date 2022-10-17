e08a5744c47ae3703f26a9b9e04bb3c552999f1c
kad1kad <k.aliu@me.com>
2022-10-17 15:25:52 +0100

    remove gradient

--- components/SkillItem.tsx
+++ components/SkillItem.tsx
@@ -7,7 +7,7 @@ type Props = {
 function SkillItem({ skill }: Props) {
   return (
     <div>
-      <p className="cursor-default pr-2 text-gradient  bg-gradient-to-br from-rose-400 via-purple-500 to-red-500 animate-gradient-xy">
+      <p className="cursor-default pr-2 text-gradient  bg-gradient-to-br from-[#58ccdc]  via-[#8263db] to-[#ff557e] animate-gradient-xy">
         {skill.title}
       </p>
     </div>
