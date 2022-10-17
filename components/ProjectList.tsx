f85cb246d61e685f6ab0661969a5abb592e485fc
kad1kad <k.aliu@me.com>
2022-10-17 12:53:43 +0100

    remove animation

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,6 +1,5 @@
 import { Project } from "../typings";
 import ProjectItem from "./ProjectItem";
-import { motion } from "framer-motion";
 
 type Props = {
   projects: Project[];
@@ -12,21 +11,11 @@ function ProjectList({ projects }: Props) {
   return (
     <div className="w-full" data-scroll-section>
       <h2 className="text-5xl font-bold w-full">Projects</h2>
-      <motion.div
-        className="md:flex gap-5 justify-center"
-        initial={{ opacity: 0 }}
-        whileInView={{
-          opacity: 1,
-          transition: {
-            duration: 0.8,
-          },
-        }}
-        viewport={{ once: true }}
-      >
+      <div className="md:flex gap-5 justify-center">
         {projects.map((project) => (
           <ProjectItem key={project._id} project={project} />
         ))}
-      </motion.div>
+      </div>
     </div>
   );
 }
