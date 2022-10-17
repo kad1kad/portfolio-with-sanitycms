10c6ccc7b71a40de961149a9d433297b9faebfdc
kad1kad <k.aliu@me.com>
2022-10-17 12:53:53 +0100

    remove animation

--- components/SkillList.tsx
+++ components/SkillList.tsx
@@ -13,15 +13,15 @@ function SkillList({ skills }: Props) {
   const yValue01 = useTransform(scrollYProgress, [0, 0.5, 0.7], [0, -50, 0]);
   const yValue02 = useTransform(scrollYProgress, [0.3, 0.75, 1], [-50, -50, 0]);
 
-  const xRange = [1, 0.7, 0.2];
-  const opacityRange = [0, 0.7, 1];
-  const opacity = useTransform(scrollYProgress, xRange, opacityRange);
-
   return (
-    <div className="overflow-hidden py-3 relative" data-scroll-section>
+    <div
+      className="overflow-hidden py-3 relative"
+      data-scroll-section
+      data-scroll-class="skillList"
+    >
       <motion.h3
         className="font-extrabold text-5xl mb-5 pt-7"
-        style={{ opacity, y: yValue02 }}
+        style={{ y: yValue02 }}
         data-scroll
         data-scroll-speed={1}
       >
@@ -30,14 +30,6 @@ function SkillList({ skills }: Props) {
 
       <motion.div
         className="flex gap-2 flex-wrap text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-rose-400 via-purple-500 to-red-500 animate-gradient-xy"
-        initial={{ opacity: 0 }}
-        whileInView={{
-          opacity: 1,
-          transition: {
-            duration: 1,
-          },
-        }}
-        viewport={{ once: true }}
         style={{ y: yValue01 }}
         data-scroll
         data-scroll-speed={2}
